7f24eaf6f43717a312d30b51774ccc06a8c2b178
subin4693 <46subin@gmail.com>
2024-07-05 09:01:57 +0530

    signin signup page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,41 @@ import ClientAppLayout from "./components/ClientAppLayout";
 import ClientDashboard from "./components/ClientDashboard";
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Dashboard from "./userPages/Dashboard";
+import Signup from "./pages/auth/Signup";
+import Signin from "./pages/auth/Signin";
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#41B06E",
+    palette: {
+        primary: {
+            main: "#41B06E",
+        },
+        secondary: {
+            main: "#dc004e",
+        },
     },
-    secondary: {
-      main: "#dc004e",
-    },
-  },
 });
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/user">
-            <Route path="" element={<Dashboard />} />
-          </Route>
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/auth">
+                        <Route path="signin" element={<Signin />} />
+                        <Route path="signup" element={<Signup />} />
+                    </Route>
+                    <Route path="/user">
+                        <Route path="" element={<Dashboard />} />
+                    </Route>
 
-          <Route path="/client" element={<ClientAppLayout />}>
-            <Route path="dash" element={<ClientDashboard />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
-  );
+                    <Route path="/client" element={<ClientAppLayout />}>
+                        <Route path="dash" element={<ClientDashboard />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 }
 
 export default App;
