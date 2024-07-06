129165cf179c97b4b1b88745cec1c08323577a87
subin4693 <46subin@gmail.com>
2024-07-06 14:04:17 +0530

    auth pages added

--- src/userComponents/MainContent.jsx
+++ src/userComponents/MainContent.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
-import { Routes, Route } from 'react-router-dom';
-import BookedEvents from '../pages/BookedEvents';
-import Settings from '../pages/Settings';
-import OpenDashboard from '../pages/OpenDashboard';
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+// import BookedEvents from "../pages/BookedEvents";
+// import Settings from "../pages/Settings";
+// import OpenDashboard from "../pages/OpenDashboard";
 
 const MainContent = () => (
-  <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh',position: 'relative', top: -200 }}>
-    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-      <Routes>
-        <Route path="/events" element={<BookedEvents />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/" element={<OpenDashboard />} />
-        {/* Add more routes as needed */}
-      </Routes>
+    <Box
+        sx={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "100vh",
+            position: "relative",
+            top: -200,
+        }}
+    >
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            <Routes>
+                {/* <Route path="/events" element={<BookedEvents />} /> */}
+                {/* <Route path="/settings" element={<Settings />} /> */}
+                {/* <Route path="/" element={<OpenDashboard />} /> */}
+                {/* Add more routes as needed */}
+            </Routes>
+        </Box>
     </Box>
-  </Box>
 );
 
 export default MainContent;
