9193199ace7750fbb344905b2e8f669c4b94b710
subin4693 <46subin@gmail.com>
2024-07-16 18:39:33 +0530

    client private route corrected

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -4,7 +4,11 @@ import { useSelector } from "react-redux";
 
 export const PrivateRoute = () => {
   const { client } = useSelector((state) => state.client);
+  console.log("******************");
+  console.log(client);
+  console.log("******************");
 
+  if (!client) return <Navigate to="/client" />;
   const { user } = useSelector((state) => state.user);
   console.log(user);
   if (!user?.id) return <Navigate to="/" />;
