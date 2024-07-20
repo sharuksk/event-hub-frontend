f707644fc3c603cfccee5e495e3b561bb33919eb
subin4693 <46subin@gmail.com>
2024-07-20 13:41:47 +0530

    Event details page changed

--- src/userPages/EventDetails.jsx
+++ src/userPages/EventDetails.jsx
@@ -49,150 +49,118 @@ const EventDetails = () => {
 		getData();
 	}, []);
 	return (
-		<Box
-			sx={{
-				p: 0,
-				display: "flex",
-				flexDirection: "column",
-				position: "relative",
-			}}
-		>
-			<Box
-				sx={{
-					mt: 9,
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "space-between",
-					px: 10,
-				}}
-			>
-				<Typography
-					variant="h4"
-					gutterBottom
-					color="whitesmoke"
-					sx={{ mt: 0 }}
+		<div className="mt-20 max-w-[1200px] ml-20 text-foreground">
+			<div className="flex gap-10">
+				<button
+					onClick={() => setDetails("venue")}
+					className={`bg-input p-1 rounded hover:bg-primary hover:text-white text-center ${itemName == "venue" && " bg-primary text-white "}`}
 				>
-					Event Details
-				</Typography>
-			</Box>
-
-			<div className="mt-20 max-w-[1200px] mr-20 text-foreground">
-				<div className="flex gap-10">
-					<button
-						onClick={() => setDetails("venue")}
-						className={`bg-input p-1 rounded hover:bg-primary hover:text-white text-center ${itemName == "venue" && " bg-primary text-white "}`}
+					Venue
+				</button>
+				<button
+					onClick={() => setDetails("decoration")}
+					className={`bg-input p-1 rounded hover:bg-primary hover:text-white text-center ${itemName == "decoration" && " bg-primary text-white "}`}
+				>
+					Decoration
+				</button>
+				<button
+					onClick={() => setDetails("catering")}
+					className={`bg-input p-1 rounded hover:bg-primary hover:text-white text-center ${itemName == "catering" && " bg-primary text-white "}`}
+				>
+					Catering
+				</button>
+				<button
+					onClick={() => setDetails("photograph")}
+					className={`bg-input p-1 rounded hover:bg-primary hover:text-white text-center ${itemName == "photography" && " bg-primary text-white "}`}
+				>
+					Photography
+				</button>
+				<button
+					onClick={() => setDetails("organizer")}
+					className={`bg-input p-1 rounded hover:bg-primary hover:text-white text-center ${itemName == "organizer" && " bg-primary text-white "}`}
+				>
+					Organizer
+				</button>
+			</div>
+			<div className="mt-10 flex gap-10">
+				<div className="w-1/2 space-y-5">
+					<div className="bg-input rounded-[25px] p-3">
+						{console.log(event[0]?.itemId)}
+						{event[0]?.itemId?.name}
+					</div>
+					<div className="bg-input rounded-[25px] p-3">
+						{event[0]?.itemId?.description}
+					</div>
+					<div className="bg-input rounded-[25px] p-3">
+						{event[0]?.itemId?.contactInfo}
+					</div>
+					<div className="bg-input rounded-[25px] h-[300px] overflow-hidden p-3 ">
+						<img
+							src={`data:image/jpeg;base64,${event[0]?.item.images[0].data}`}
+							className="object-fit w-full h-full rounded-[15px]"
+						/>
+					</div>
+					<div
+						className={` rounded-[25px] p-3 capitalize text-black ${event[0]?.isConfirmed ? "bg-green-300" : "bg-red-300"}`}
 					>
-						Venue
-					</button>
-					<button
-						onClick={() => setDetails("decoration")}
-						className={`bg-input p-1 rounded hover:bg-primary hover:text-white text-center ${itemName == "decoration" && " bg-primary text-white "}`}
-					>
-						Decoration
-					</button>
-					<button
-						onClick={() => setDetails("catering")}
-						className={`bg-input p-1 rounded hover:bg-primary hover:text-white text-center ${itemName == "catering" && " bg-primary text-white "}`}
-					>
-						Catering
-					</button>
-					<button
-						onClick={() => setDetails("photography")}
-						className={`bg-input p-1 rounded hover:bg-primary hover:text-white text-center ${itemName == "photography" && " bg-primary text-white "}`}
-					>
-						Photography
-					</button>
-					<button
-						onClick={() => setDetails("organizer")}
-						className={`bg-input p-1 rounded hover:bg-primary hover:text-white text-center ${itemName == "organizer" && " bg-primary text-white "}`}
-					>
-						Organizer
-					</button>
+						{event[0]?.isConfirmed ? "Confirmed" : "Booked"}
+					</div>
 				</div>
-				<div className="mt-10 flex gap-10">
-					<div className="w-1/2 space-y-5">
+				<div className="w-1/2">
+					{itemName == "venue" && (
 						<div className="bg-input rounded-[25px] p-3">
-							{event[0]?.itemId?.name}
+							{" "}
+							{event[0]?.itemId?.location}
 						</div>
+					)}
+					{itemName == "catering" && (
+						<ol className="bg-input rounded-[25px] p-3">
+							{event[0]?.itemId.menuOptions[0]
+								.split(",")
+								.map((val, index) => (
+									<li key={val}>{index + 1 + " " + val}</li>
+								))}
+						</ol>
+					)}
+					{itemName == "photograph" && (
+						<ol className="bg-input rounded-[25px] p-3">
+							{event[0]?.itemId?.portfolio[0]
+								?.split(",")
+								.map((val, index) => (
+									<li key={val}>{index + 1 + " " + val}</li>
+								))}
+						</ol>
+					)}
+					{itemName == "decoration" && (
+						<div className="space-y-5">
+							<div className="bg-input rounded-[25px] p-3 h-[300px]">
+								<img
+									src={`data:image/jpeg;base64,${event[0]?.item.images[1]?.data}`}
+									className="object-fit w-full h-full rounded-[15px]"
+								/>
+							</div>
+							<div className="bg-input rounded-[25px] p-3 h-[300px]">
+								<img
+									src={`data:image/jpeg;base64,${event[0]?.item.images[2]?.data}`}
+									className="object-fit w-full h-full rounded-[15px]"
+								/>
+							</div>
+							<div className="bg-input rounded-[25px] p-3 h-[300px]">
+								<img
+									src={`data:image/jpeg;base64,${event[0]?.item.images[3]?.data}`}
+									className="object-fit w-full h-full rounded-[15px]"
+								/>
+							</div>
+						</div>
+					)}
+					{itemName == "organizer" && (
 						<div className="bg-input rounded-[25px] p-3">
-							{event[0]?.itemId?.description}
+							organizer
 						</div>
-						<div className="bg-input rounded-[25px] p-3">
-							{event[0]?.itemId?.contactInfo}
-						</div>
-						<div className="bg-input rounded-[25px] h-[300px] overflow-hidden p-3 ">
-							<img
-								src={`data:image/jpeg;base64,${event[0]?.item.images[0].data}`}
-								className="object-fit w-full h-full rounded-[15px]"
-							/>
-						</div>
-						<div
-							className={` rounded-[25px] p-3 capitalize text-black ${event[0]?.isConfirmed ? "bg-green-300" : "bg-red-300"}`}
-						>
-							{console.log(event[0]?.isConfirmed)}
-							{event[0]?.isConfirmed ? "Confirmed" : "Booked"}
-						</div>
-					</div>
-					<div className="w-1/2">
-						{itemName == "venue" && (
-							<div className="bg-input rounded-[25px] p-3">
-								{" "}
-								{event[0]?.itemId?.location}
-							</div>
-						)}
-						{itemName == "catering" && (
-							<ol className="bg-input rounded-[25px] p-3">
-								{event[0]?.itemId.menuOptions[0]
-									.split(",")
-									.map((val, index) => (
-										<li key={val}>
-											{index + 1 + " " + val}
-										</li>
-									))}
-							</ol>
-						)}
-						{itemName == "photography" && (
-							<ol className="bg-input rounded-[25px] p-3">
-								{event[0]?.itemId.portfolio[0]
-									.split(",")
-									.map((val, index) => (
-										<li key={val}>
-											{index + 1 + " " + val}
-										</li>
-									))}
-							</ol>
-						)}
-						{itemName == "decoration" && (
-							<div className="space-y-5">
-								<div className="bg-input rounded-[25px] p-3 h-[300px]">
-									<img
-										src={`data:image/jpeg;base64,${event[0]?.item.images[1]?.data}`}
-										className="object-fit w-full h-full rounded-[15px]"
-									/>
-								</div>
-								<div className="bg-input rounded-[25px] p-3 h-[300px]">
-									<img
-										src={`data:image/jpeg;base64,${event[0]?.item.images[2]?.data}`}
-										className="object-fit w-full h-full rounded-[15px]"
-									/>
-								</div>
-								<div className="bg-input rounded-[25px] p-3 h-[300px]">
-									<img
-										src={`data:image/jpeg;base64,${event[0]?.item.images[3]?.data}`}
-										className="object-fit w-full h-full rounded-[15px]"
-									/>
-								</div>
-							</div>
-						)}
-						{itemName == "organizer" && (
-							<div className="bg-input rounded-[25px] p-3">
-								organizer
-							</div>
-						)}
-					</div>
+					)}
 				</div>
 			</div>
-		</Box>
+		</div>
 	);
 };
 
